0adb4ab65bcafe0d23ef0aa974808d6fb9e25524
AlexanderOllman <alexander.ollman@gmail.com>
2024-09-17 12:59:27 -0700

    fixed port

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def get_clusterservingruntime():
     return jsonify(json.loads(csr_data))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='8080')
