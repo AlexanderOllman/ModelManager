ba17dce5318a199f110f6368fe3f7739c89ab0e0
AlexanderOllman <alexander.ollman@gmail.com>
2024-12-13 10:29:13 -0800

    updated to 0.0.3

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo "Pulling latest..."
 git pull
 
 echo "Building image..."
-docker build -t fheonix/model-manager:0.0.2 .
+docker build -t fheonix/model-manager:0.0.3 .
 
 echo "Pushing image..."
-docker push fheonix/model-manager:0.0.2
+docker push fheonix/model-manager:0.0.3
 
 echo "Done."
 
