c8ffd52d0cad8b20252b262979c0b5460261c455
AlexanderOllman <alexander.ollman@gmail.com>
2024-12-14 10:06:51 -0800

    updated docker repo to kanso

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ echo "Pulling latest..."
 git pull
 
 echo "Building image..."
-docker build -t fheonix/model-manager:0.0.3 .
+docker build -t fheonix/kanso:0.0.1 .
 
 echo "Pushing image..."
-docker push fheonix/model-manager:0.0.3
+docker push fheonix/kanso:0.0.1
 
 echo "Done."
 
